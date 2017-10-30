f199069ac85fcdcc5517d89371dd1ebefcd719d0
Derek <derek.schaefer@gmail.com>
2017-10-30 18:38:14 -0500

    remove autogenerated stuff from the root module

--- lib/tplink.ex
+++ lib/tplink.ex
@@ -1,18 +1,2 @@
 defmodule TPLink do
-  @moduledoc """
-  Documentation for TPLink.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TPLink.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/tplink_test.exs
+++ test/tplink_test.exs
@@ -1,8 +1,2 @@
 defmodule TPLinkTest do
-  use ExUnit.Case
-  doctest TPLink
-
-  test "greets the world" do
-    assert TPLink.hello() == :world
-  end
 end
