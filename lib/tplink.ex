f1bf483522f68320636eeff7ab45360496f45141
Derek <derek.schaefer@gmail.com>
2017-10-30 17:52:52 -0500

    init

---
+++ lib/tplink.ex
@@ -0,0 +1,18 @@
+defmodule Tplink do
+  @moduledoc """
+  Documentation for Tplink.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tplink.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tplink_test.exs
@@ -0,0 +1,8 @@
+defmodule TplinkTest do
+  use ExUnit.Case
+  doctest Tplink
+
+  test "greets the world" do
+    assert Tplink.hello() == :world
+  end
+end
