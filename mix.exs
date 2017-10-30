45bb94324bc686361f20e98fed5fd46420f000ea
Derek <derek.schaefer@gmail.com>
2017-10-30 17:58:10 -0500

    add initial hex package information

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule Tplink.Mixfile do
   def project do
     [
       app: :tplink,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +25,18 @@ defmodule Tplink.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    "An Elixir library for interacting with TPLink devices"
+  end
+
+  defp package do
+    [
+      maintainers: ["Derek Schaefer"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/derek-schaefer/tplink"}
     ]
   end
 end
