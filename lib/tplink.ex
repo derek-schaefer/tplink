2892ddec810e18534d97b73f1cbc41d294485d85
Derek <derek.schaefer@gmail.com>
2017-10-30 22:06:09 -0500

    add initial root module device function

--- lib/tplink.ex
+++ lib/tplink.ex
@@ -1,2 +1,9 @@
 defmodule TPLink do
+  alias TPLink.{Network, Device}
+
+  def device(address) do
+    address
+    |> Network.query_udp(Device.sysinfo_query)
+    |> Device.init
+  end
 end
--- test/tplink_test.exs
+++ test/tplink_test.exs
@@ -1,2 +1,9 @@
 defmodule TPLinkTest do
+  use ExUnit.Case
+
+  describe "#device" do
+    @tag :external
+    test "" do
+    end
+  end
 end
